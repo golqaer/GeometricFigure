151a1f4b3cfd42ad7e069d2727c08943bb5953eb
Golqaer <golqaer@gmail.com>
2024-06-30 12:54:53 +0300

    first init

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ConsoleApp
+﻿using GeometricFigure;
+using GeometricFigure.Common;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Point 
+                  a = new Point(0,0)
+                , b = new Point(10,10)
+                , c = new Point(0,90)
+                ;
+
+            var triangle = new Triangle(a, b, c);
+
+            var right = triangle.IsRight;
+            var square = triangle.GetSquare();
         }
     }
 }
