acf98ea0c9500f840d361a710c4168f5d31aca3e
Golqaer <golqaer@gmail.com>
2024-06-30 16:41:58 +0300

    some additions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApp.Data;
 using GeometricFigure;
 using GeometricFigure.Common;
+using System.Net.WebSockets;
 
 namespace ConsoleApp
 {
@@ -9,6 +10,17 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Context context = new Context();
+
+            var result = context.Products.Select(p => new
+            {
+                ProductTitle = p.Title,
+                Categories = p.Categories.Select(c => c.Title).DefaultIfEmpty(),
+            });
+
+            foreach (var r in result)
+            {
+                Console.WriteLine($"Product title: {r.ProductTitle};\t Categories: {string.Join(", ", r.Categories)}");
+            }
         }
     }
 }
